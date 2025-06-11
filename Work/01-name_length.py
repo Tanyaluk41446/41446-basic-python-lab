b4949a8225074a2aceb2d668d4dda5ae8cb8e1b7
Tanyaluk41446 <tanyaluk41446@gmail.com>
2025-06-11 10:29:00 +0700

    work1
    python

---
+++ Work/01-name_length.py
@@ -0,0 +1,4 @@
+name = input("What is your name :")
+sur = input("What is your surname :")
+print(len(name))
+print(len(sur))
---
+++ Work/02-word_concat.py
@@ -0,0 +1,3 @@
+First = input ("First word :")
+Second = input ("Second word :")
+print((First),"+",(Second))
