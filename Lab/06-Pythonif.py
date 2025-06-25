abe1970efb1c95218e90cee338bde9fd58e49500
Tanyaluk41446 <tanyaluk41446@gmail.com>
2025-06-25 22:37:00 +0700

    Conditional-Python
    ส่งงานวิธีการ use if else elif ค่ะ

---
+++ Lab/06-Pythonif.py
@@ -0,0 +1,7 @@
+num = int(input("Enter you numner"))
+if (num <= 10):
+    print("ไม่ผ่าน")
+elif (num <= 20):
+    print("ปรับปรุง")
+elif (num <= 30):
+    print("ดีมาก")
---
+++ Lab/08-PythonIf.py
@@ -0,0 +1,6 @@
+number = int(input("Enter your number"))
+print(number%2)
+if(number%2 == 1):
+    print("เลขคู่")
+else:
+    print("เลขคี่")
